7e9e91903b0c329952781ee20181f60e35a703e9
Ginso02 <115461473+Ginso02@users.noreply.github.com>
2024-10-25 01:43:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'  # 替换为你的 MySQL 用户名
-    MYSQL_PASSWORD = 'wxy20031002'  # 替换为你的 MySQL 密码
+    MYSQL_PASSWORD = 'root'  # 替换为你的 MySQL 密码
     MYSQL_DB = 'flaskdemo_user'
